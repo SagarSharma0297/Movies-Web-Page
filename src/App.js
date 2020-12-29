b3da9cbed9c3405e64279d5db43b02783b455c51
“SagarSharma0297” <sharma.sagar0297@gmail.com>
2020-12-29 16:30:25 +0530

    first commit for add code to repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import classes from './App.module.css';
+import Card from './components/Card';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      apiData:[],
+      loader:true
+     }
+  }
+
+  componentDidMount(){
+    fetch('https://www.omdbapi.com/?apikey=45f0782a&s=war')
+  .then(response => response.json())
+  .then(data => this.setState({apiData:data.Search,loader:false}))
+  }
+  render() { 
+    let {apiData,loader} = this.state;
+    return (
+    <>
+    <Header/>
+    <div className={classes.cardGrid}>
+    {loader? <div className={classes.loader}></div> : apiData.map((item,pos) => <Card key={pos} src={item.Poster} title={item.Title}/>)}
     </div>
-  );
+    <Footer/>
+    </>
+     );
+  }
 }
-
+ 
 export default App;
